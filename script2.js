56472f6037db94afeac8e173610c1885074ef2e1
Nic Oakley <74666308+NicOakley@users.noreply.github.com>
2022-03-28 17:03:59 -0300

    commit

--- script2.js
+++ script2.js
@@ -50,7 +50,6 @@ function getRecipe(id) {
         var left_content_string = "";
         var ingredient_string = "";
         var right_content_string = "";
-        var ingredient_content_string = "";
         var n = 0;
 
 
@@ -58,20 +57,18 @@ function getRecipe(id) {
             //iterate over the object['ingredients'] array, building an HTML string
             //if n is greater than 6, then a new row will be created
             for(n = 0; n < object['ingredients'].length; n++) {
-                ingredient_string += "<p class='text-left ml-5'>"+object['ingredients'][n]+"</p>";
+                ingredient_string += object['ingredients'][n]+", ";
            }
 
            //ingredients
-           ingredient_content_string += "<h5 class='mt-4 mb-2 ml-5 text-left' style='font-weight: bold'>Ingredients</h5><div class='text-left ml-5' style='min-height: 140px;'>"+ ingredient_string + "</div>";
-
-
 
             //build the left_content_string for the left page
             //title
             left_content_string += "<h2 style='font-family: pacifico;' class='recipe-name'>"+object['recipe_name']+"</h2>";
             //description
             left_content_string += "<p style='font-family: pacifico;' class='recipe-desc mt-3'>"+object['desc']+"</p><hr style='height: 1px; width: 400px;'>";
-            
+            //ingredients
+            left_content_string += "<h5 class='mt-4 mb-2 ml-5 text-left' style='font-weight: bold'>Ingredients</h5><div class='text-left ml-5' style='min-height: 80px;'>"+ ingredient_string + "</div>";
             //image
             left_content_string += "<img class='img-fluid mt-4 rounded' style='max-height: 250px; min-height:250px; outline: 2px solid #836148; opacity: 80%;' src='"+object['image']+"'>";
 
@@ -84,7 +81,6 @@ function getRecipe(id) {
        //set the HTML in the div on the cookbook
        $("#content-1").html(left_content_string);
        $("#content-2").html(right_content_string);
-       $('#ingredients').html(ingredient_content_string);
        
 
        if($('.recipe-name').text() == ''){
